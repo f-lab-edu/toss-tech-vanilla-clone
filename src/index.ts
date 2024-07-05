f7171950c6d22b96d629ebc6b9a6ef95167f8979
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-07-05 21:24:05 +0900

    feat: implement navbar

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
+import { Navbar } from './components/Navbar.ts';
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  const sp1 = document.createElement('span');
-  const sp1_content = document.createTextNode('new span element.');
-  sp1.appendChild(sp1_content);
-  root?.appendChild(sp1);
+  root?.appendChild(Navbar);
 });
