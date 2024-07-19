d47089ee484c2f327ac71a1bb5e193c0fdcd2571
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-07-19 15:02:48 +0900

    refactor: add event listener to page using event propagation

--- src/core/router/index.ts
+++ src/core/router/index.ts
@@ -3,20 +3,19 @@ import { Router } from './types/router';
 /**
  * 라우터 생성에 필요한 속성을 정의합니다.
  * @interface CreateRouterProps
- * @property {{ [key: string]: Element }} routes - 경로와 해당 컴포넌트 매핑
+ * @property {{ [key: string]: HTMLElement }} routes - 경로와 해당 컴포넌트 매핑
  * @property {HTMLElement} root - 라우터가 작동할 루트 엘리먼트
+ * @property {HTMLElement} errorPage - 에러 페이지 엘리먼트
  */
 interface CreateRouterProps {
-  routes: { [key: string]: Element };
+  routes: { [key: string]: HTMLElement };
   root: HTMLElement;
   errorPage: HTMLElement;
 }
 
 /**
  * 라우터를 생성합니다.
- * @function
- * @name createRouter
- * @param {ICreateRouterProps} props - 라우터 생성에 필요한 속성
+ * @param {CreateRouterProps} props - 라우터 생성에 필요한 속성
  * @returns {Router} - 라우터 인터페이스를 반환합니다.
  */
 export function createRouter({
@@ -24,29 +23,24 @@ export function createRouter({
   root,
   errorPage,
 }: CreateRouterProps): Router {
-  const routeMap: { [key: string]: Element } = routes;
+  const routeMap: { [key: string]: HTMLElement } = routes;
 
   /**
    * 주어진 경로에 따라 적절한 페이지 컴포넌트를 렌더링합니다.
-   * @function
-   * @name render
    * @param {string} path - 렌더링할 경로
    */
   function render(path: string) {
-    const newPageComponent = routeMap[path] || errorPage;
-    if (!newPageComponent) return;
+    const newPage = routeMap[path] || errorPage;
     const $page = document.querySelector('.page');
     if ($page) {
       root.removeChild($page);
     }
-    root.appendChild(newPageComponent);
-    bindEventListener();
+    bindEventListener(newPage);
+    root.appendChild(newPage);
   }
 
   /**
    * 주어진 경로로 브라우저 히스토리를 업데이트하고 페이지를 렌더링합니다.
-   * @function
-   * @name push
    * @param {string} path - 이동할 경로
    */
   function push(path: string) {
@@ -56,8 +50,6 @@ export function createRouter({
 
   /**
    * 주어진 경로로 브라우저 히스토리를 대체하고 페이지를 렌더링합니다.
-   * @function
-   * @name replace
    * @param {string} path - 대체할 경로
    */
   function replace(path: string) {
@@ -67,8 +59,6 @@ export function createRouter({
 
   /**
    * 브라우저 히스토리에서 뒤로 이동합니다.
-   * @function
-   * @name back
    */
   function back() {
     history.back();
@@ -76,8 +66,6 @@ export function createRouter({
 
   /**
    * 브라우저 히스토리에서 앞으로 이동합니다.
-   * @function
-   * @name forward
    */
   function forward() {
     history.forward();
@@ -85,8 +73,6 @@ export function createRouter({
 
   /**
    * 브라우저 히스토리에서 주어진 만큼 이동합니다.
-   * @function
-   * @name go
    * @param {number} delta - 이동할 히스토리 스택의 거리
    */
   function go(delta: number) {
@@ -95,44 +81,29 @@ export function createRouter({
 
   /**
    * 모든 링크(a 태그들)에서 경로 속성을 찾아 이벤트 리스너를 추가합니다.
-   * @function
-   * @name bindEventListener
+   * @param {HTMLElement} page - 이벤트 리스너를 추가할 페이지 엘리먼트
    */
-  function bindEventListener() {
-    /**
-     * 클릭한 요소에 a tag으로 감싸져 있는지 확인하고, 있다면 이동할 path를 반환한다.
-     * @param {HTMLElement} element
-     * @returns {string | null} - 페이지 이동 path
-     */
-    function hasLink(element: HTMLElement | null): string | null {
-      while (element) {
-        if (element.tagName.toLowerCase() === 'a') {
-          return element.getAttribute('href');
-        }
-        element = element.parentElement;
-      }
-      return null;
-    }
-
-    root.addEventListener('click', (e) => {
+  function bindEventListener(page: HTMLElement) {
+    page.addEventListener('click', (e) => {
       const target = e.target as HTMLElement;
-      const path = hasLink(target);
-      if (path) {
+      const link = target.closest('a');
+      if (link) {
         e.preventDefault();
-        push(path);
+        const path = link.getAttribute('href') || '/404';
+        if (path !== window.location.pathname) {
+          push(path);
+        }
       }
     });
   }
 
   // back, forward를 위한 root popstate eventListener 추가
   window.addEventListener('popstate', () => {
-    const path = document.location.pathname;
-    render(path);
+    render(window.location.pathname);
   });
 
   // 초기 페이지 로드
-  const path = window.location.pathname;
-  push(path);
+  push(window.location.pathname);
 
   return { push, replace, back, forward, go };
 }
